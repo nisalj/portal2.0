55f0e11609d8cc4013030a89e427b8938a7a68ed
Nisal Jayalath <nisaljayalath@gmail.com>
2019-07-04 01:34:52 +1000

    First commit

---
+++ planning.js
@@ -0,0 +1,8 @@
+var map;
+
+function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat:-33.814451, lng:151.171332},
+          zoom: 14
+        });
+}
---
+++ telemetry.js
@@ -0,0 +1,7 @@
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat:-33.814451, lng:151.171332},
+          zoom: 14
+        });
+    }
