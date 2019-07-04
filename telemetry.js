df7dc02071b0ef90f8e4e4ef1d796403ee0581c6
Nisal Jayalath <nisaljayalath@gmail.com>
2019-07-05 01:53:34 +1000

    Location tracking now working. Tested on google chrome hosted on localhost over HTTP. See: https://developers.google.com/web/updates/2016/04/geolocation-on-secure-contexts-only. Updated video streams

--- telemetry.js
+++ telemetry.js
@@ -1,7 +1,76 @@
-var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat:-33.814451, lng:151.171332},
-          zoom: 14
-        });
-    }
+let map;
+let status;
+let path; 
+let robotpath;
+let locating = "Locating";
+let tracking =  "Tracking";
+let unable = "Unable to retrieve your location"
+function success(position){
+  const lat  = position.coords.latitude;
+  const long = position.coords.longitude;
+ // console.log(latitude);
+  plotPath(lat,long);
+}
+
+function plotPath(lat, long) {
+  let path = robotpath.getPath();
+
+  latlng= new google.maps.LatLng(lat, long);
+ // latlng2 = new google.maps.LatLng(37.772, -122.214); 
+  path.push(latlng);
+//  path.push(latlng2);
+
+  
+}
+
+function error() {
+  status.textContent = unable;
+}
+function getLocation() {
+
+  if (!navigator.geolocation) {
+    status.textContent = 'Geolocation is not supported by your browser';
+  } else {
+    if(status.textContent != tracking && status.textContent != unable)
+    status.textContent = locating;
+    navigator.geolocation.getCurrentPosition(success, error);
+    status.textContent = tracking;
+
+  }
+
+}
+
+
+function initMap() {
+
+
+    map = new google.maps.Map(document.getElementById('map'), 
+      {
+        center: {lat:-33.814451, lng:151.171332},
+        zoom: 14
+      });
+
+    path = new google.maps.MVCArray([
+    ]);
+
+    robotpath = new google.maps.Polyline({
+      path: path,
+      geodesic: true,
+      strokeColor: '#FF0000',
+      strokeOpacity: 1.0,
+      strokeWeight: 2
+    });
+    
+    robotpath.setMap(map);
+
+    setInterval(getLocation, 100)
+
+}
+
+
+window.onload = function() {
+  status = document.getElementById('main-header')
+  initMap(); 
+ // getLocation();
+}
+
